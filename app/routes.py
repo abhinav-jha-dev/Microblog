753241ad9c3955728be11c911a2c004174aae960
Abhinav Jha <abhinav.jha@tm.chd>
2018-12-13 15:23:04 +0530

    Completed Chapter 5

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,14 @@
-from flask import render_template, flash, redirect
-from app import app
-from app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for, request
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from flask_login import current_user, login_user, logout_user, login_required # for managing the user session, login and logout
+from app.models import User # initiating the user model
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user={'username':'miguel'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -16,26 +19,40 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home',user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
-'''
-The first new thing in this version is the methods argument in the route decorator. 
-This tells Flask that this view function accepts GET and POST requests,
-overriding the default, which is to accept only GET requests.
-The HTTP protocol states that GET requests are those that return information to the
-client (the web browser in this case). All the requests in the application so far are
-of this type. POST requests are typically used when the browser submits form data to the
-server (in reality GET requests can also be used for this purpose, but it is not a recommended practice). 
-
-The flash() function is a useful way to show a message to the user. A lot of applications
-use this technique to let the user know if some action has been successful or not.
-'''
-    
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form= LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+    if current_user.is_authenticated:
         return redirect(url_for('index'))
-    return render_template('login.html',title='Sign In', form=form)
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
